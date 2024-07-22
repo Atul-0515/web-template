e707878f615540b6f37f38d63c03f6817740acd3
Atul Kumar <atulkr1505@gmail.com>
2024-07-22 09:09:32 +0530

    added prisma orm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+
+
 
 
 export default function Home() {
-  return <>
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
-
-  </>
+  return <main>
+    <h1>Welcom to Nextjs + shadcnUI starting template</h1>
+  </main>
 }
